374d34bbbb970f24c9db4e1824144409b4b769e8
Pranav Agale <agalepranav2015@gmail.com>
2025-02-28 15:23:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-const texts = [
-    "Programmer",
-    "Full Stack Developer",
-    "Blockchain Developer",
-    "Open Source Lover",
-    "GSoC'23 Contributor"
-];
-
-let index = 0;
-const dynamicTextElement = document.querySelector('.dynamic-text');
-
-function rotateText() {
-    dynamicTextElement.style.transform = `translateY(-${index * 2.5}rem)`;
-    index = (index + 1) % texts.length;
-}
-
-setInterval(rotateText, 2000); // Change text every 2 seconds
-
 
